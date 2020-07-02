207aafe3fce02e2472c26d5adfc0ab0014c5bcb6
patriciaClares <dev.patricia.c@gmail.com>
2020-07-02 19:28:05 -0300

    add index

--- src/components/shoppingCart/index.js
+++ src/components/shoppingCart/index.js
@@ -1,10 +1,36 @@
-import React from 'react'
-import *  as S from './styled.js'
+import React, { useState, useEffect } from "react";
+import * as S from "./styled.js";
 
-export default function ShoppingCart(){
+export default function ShoppingCart({ pokemonCart, totalPrice }) {
 
-    return (
-        <>
-        </>
-    );
+  useEffect(() => {
+
+    if (pokemonCart) {
+      pokemonCart.forEach((element) => {
+        totalPrice(element.order)
+      });
+    }
+  }, [pokemonCart]);
+
+  return (
+    <>
+      <div style={{display: 'flex', flexDirection: 'column',marginBottom:'10px'}}>
+        {pokemonCart.map((pokemonCurrent, index) => {
+          return (
+            <div
+              style={{ display: "flex", justifyContent: "space-between", alignItems:'center', padding:'0 20px' }}
+              key={index}
+            >
+                <div style={{display: 'flex', alignItems:'center'}}>
+                    <img src={pokemonCurrent.sprites.front_shiny} alt="Avatar" style={{width:'50px'}} />
+                    <p>{pokemonCurrent.name}</p>
+                    
+                </div>
+              <p>R$ {pokemonCurrent.order}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
