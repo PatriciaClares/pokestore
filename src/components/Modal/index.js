181d163f3e64683a1588bee6019293a1a08bbf1e
patriciaClares <dev.patricia.c@gmail.com>
2020-07-02 19:26:53 -0300

    add Modal

---
+++ src/components/Modal/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Modal({hideModal}) {
+    return (
+        <div style={{  
+            backgroundColor: '#0E0A14ef',
+            width:'100%',
+            height:'100%',
+            top: '0',
+            position:'fixed',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            transition: '600ms',
+          }}>
+              <div style={{  
+            backgroundColor: 'white', padding:'30px 50px', display:'flex', flexDirection:'column'}}>
+                <h1 style={{paddingBottom:'20px'}}>PARABÉNS!</h1>
+                <p>Obrigada pela compra ;)</p>
+        <button onClick={() => hideModal()}>fechar</button></div>
+        
+        </div>
+    )
+}
