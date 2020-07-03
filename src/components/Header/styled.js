d8fe1da1913225602e8328d176dc62441c77ae25
patriciaClares <dev.patricia.c@gmail.com>
2020-07-02 22:14:20 -0300

    refactoring style Header

--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -7,9 +7,11 @@ export const Header = styled.div`
     justify-content: space-around;
     background-color: #e64c3c;
     box-shadow: 0 4px 8px 0 rgba(0,0,0,0.4);
+    align-items: center;
 `
 export const divSearch = styled.div`
     display: flex;
+    max-height: 30px
 `
 export const fiGithub = styled(FiGithub)`
     width: 40px;
@@ -23,17 +25,19 @@ export const fiGithub = styled(FiGithub)`
 export const inputName = styled.input`
     outline: none;
     padding: 5px; 
-    border-radius: 5px; 
     border: none;
+    border-bottom-left-radius: 5px; 
+    border-top-left-radius: 5px; 
 `
 
 export const buttonSearch = styled.button`
     align-items: center;
+    border: none;
+    border-left: 1px solid black;
+    border-top-right-radius: 5px; 
+    border-bottom-right-radius: 5px; 
     display: flex;
     outline: none ;
-    margin-left: 10px ;
-    border: none;
-    border-radius: 5px;
     padding: 0 10px 0 10px;
-    background-color: white
+    background-color: #e0e4e8
 `
